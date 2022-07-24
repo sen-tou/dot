cdbd47eb3380c1e5925121eb8fa30c8c834b2cb8
Steve <beyer_steve@outlook.de>
2022-07-24 17:11:34 +0200

    improve install script

--- README.md
+++ README.md
@@ -4,27 +4,40 @@ These are my dot files that I am currently using.
 
 **Note:** There are of course no tools in this repo. You have to install them in
 addition to using this repo. For example zsh and oh-my-zsh.
+**Note:** I do not take responsibility for lost files. Read this manual carefully
+and take a look at the source yourself. If you spot something open an issue.  Use at your own risk.
 
-## Installation
+## Installation with script
+
+Change the repo url in the script below to use https or fork it and replace with
+your own url. There is no config for this yet.
 
 ```sh
 cd ~ && curl -O https://raw.githubusercontent.com/stvbyr/dot/main/init_dotfiles.sh && chmod +x ./init_dotfiles.sh
 ```
 
 ```txt
-Usage: init_dotfiles.sh [option]
+Usage: init_dotfiles.sh [options]
 
 Options:
     -h Output usage
     -i Install the dot files, a backup of all affected files will be created
-    -b Backup dotfiles
+    -d Download dotfiles
+    -b Backup dotfiles (only works if the project has been downloaded via -d or -i)
     -V Print version
 ```
 
-`./init_dotfiles.sh -i` will install the dot files.
+A backup should be created with the `-b` option for all files that would be
+affected before installing. Use the `-d` option first to download the files.
 
-**Note:** A backup should be created with the -b option for all files that would be affected before installing. After that
-all files in this repo will replace the once in your home dir.
+`./init_dotfiles.sh -i` will install the dot files. (It will download the files
+again and sync it with your home folder)
+
+### Making it your home
+
+If you forked the project and changed the repo url in the script
+these changes are lost after the install. Makes sure to check them in and push
+them to your fork. 
 
 If you wanna create your own dotfile repo from this make sure to check your
 workspace with `dot status` and check if there are any private files that you DO
--- init_dotfiles.sh
+++ init_dotfiles.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-VERSION="$(basename $0) 0.1.0"
+VERSION="$(basename $0) 0.2.0"
 
+TMP_DIR="$HOME/tmpdotfiles"
 LOCALREPO_DIR="$HOME/.dotfiles"
+
+set -e
 
 function dot {
     git --git-dir="$LOCALREPO_DIR" --work-tree="$HOME" $1
@@ -15,9 +18,18 @@ function usage {
     printf "Options:\n"
     printf "    -h Output usage\n"
     printf "    -i Install the dot files, a backup of all affected files will be created\n"
-    printf "    -b Backup dotfiles\n"
+    printf "    -d Download dotfiles\n"
+    printf "    -b Backup dotfiles (only works if the project has been downloaded via -d or -i)\n"
     printf "    -V Print version\n"
     exit
+}
+
+# -d
+function download {
+    [ -d "$LOCALREPO_DIR" ] && rm -rf "$LOCALREPO_DIR"
+    [ -d "$TMP_DIR" ] && rm -rf "$TMP_DIR"
+
+    git clone --separate-git-dir="$LOCALREPO_DIR" git@github.com:stvbyr/dot.git $TMP_DIR   
 }
 
 # -b
@@ -25,22 +37,16 @@ function backup {
     BACKUP_FOLDER=".dotfile_backup/$(date +'%Y-%m-%d-%H-%M-%S')"
     mkdir -p "$BACKUP_FOLDER"
     dot "ls-tree -r --full-name --name-only main" |
-        xargs -I "{}" rsync "$HOME/{}" "$BACKUP_FOLDER/{}"
+        xargs -I "{}" rsync --ignore-missing-args "$HOME/{}" "$BACKUP_FOLDER/{}"
     printf "Backup created in %s\n" $BACKUP_FOLDER
     exit
 }
 
 # -i
-function install() {
-    if [[ ! -d "$LOCALREPO_DIR" || ! "$(ls -A $LOCALREPO_DIR)" ]]
-    then
-        [ -d "$LOCALREPO_DIR" ] && rm -r "$LOCALREPO_DIR"
-        git clone --separate-git-dir="$LOCALREPO_DIR" ssh://git@github.com/stvbyr/dot.git tmpdotfiles
-        rsync --recursive --verbose --exclude '.git' tmpdotfiles/ $HOME/
-        rm -r tmpdotfiles
-    else
-        dot pull 
-    fi
+function install() {    
+    download
+    rsync --recursive --verbose --exclude '.git' $TMP_DIR/ $HOME/
+    rm -r $TMP_DIR
 
     exit
 }
@@ -49,11 +55,14 @@ if [[ ${#} -eq 0 ]]; then
    usage
 fi
 
-while getopts ":hbiV" opt
+while getopts ":hdbiV" opt
 do
     case "${opt}" in
         h)
             usage
+            ;;
+        d)
+            download
             ;;
         b)
             backup
